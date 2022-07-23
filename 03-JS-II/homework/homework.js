f3d4cb7a0e37f4570c65582aa5e16bcb6b31a221
agueda1234 <mansilla.angulo@gmail.com>
2022-07-22 22:52:39 -0300

    CommitH04JSIII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -145,7 +145,7 @@ function fizzBuzz(numero) {
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
-  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
+  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1  es mayor y positivo"
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
